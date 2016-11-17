62f739c53f5c7e34178eb21ef1911f7b240581e7
ccfromstar <114413511@qq.com>
2016-11-17 16:51:35 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,12 @@ var APIKey = 'XZ14ldH1day6utemOleMmV1h';
 var SecretKey = 'f62e4f13a76413e1806a0e4c37d8aef7';
 
 exports.index = function(req, res) {
-	res.render("index");
+	var sql1 = "select * from cruise_port where txtCruiseAreaNo='hq6' limit 0,4";
+	mysql.query(sql1, function(err, rows1) {
+		res.render("index",{
+			ports:rows1
+		});
+	});
 }
 
 exports.servicedo = function(req, res) {
@@ -14,7 +19,7 @@ exports.servicedo = function(req, res) {
 	var sql = req.params.sql;
 	if(sql == "TTS") {
 		console.log('TTS run');
-		
+		var id = req.param("id");
 		var url1 = 'https://openapi.baidu.com/oauth/2.0/token';
 		request({
 		    url: url1,
@@ -27,34 +32,13 @@ exports.servicedo = function(req, res) {
 		}, function(err, response, body) {
 		    if (!err && response.statusCode == 200) {
 		        var access_token = JSON.parse(body).access_token;
-		        var tex = '文本内容1';
-		        var cuid = '192.168.1.2';
-		        var url2 = 'http://tsn.baidu.com/text2audio?tex='+tex+'&lan=zh&cuid='+cuid+'&ctp=1&tok='+access_token;
-		        console.log(url2);
-		        res.send(url2);
-		        /*
-		        request(url2,function(error,response,body){
-				    if(!error && response.statusCode == 200){
-				        //输出返回的内容
-				        console.log(response.headers['content-type']);
-				        console.log(response.headers);
-				        res.pipe(response);
-				        //console.log(response.body);
-				        
-				        fs.writeFile('public/upload/1.mp3', response, function (err) {
-					        if (err) throw err;
-					        console.log("Export Account Success!");
-					    });
-					   
-					   	var savedAudio = fs.createWriteStream('public/upload/1.mp3');
-					    savedAudio.on('finish', function(){
-					      console.log('savedAudio finished.');
-					      process.exit(0);
-					    })
-					    body.pipe(savedAudio);
-					   
-				    }
-				});*/
+		        var sql1 = 'select txtPortCityInfo from cruise_port where id = '+id;
+		        mysql.query(sql1, function(err, rows1) {
+					var tex = rows1[0].txtPortCityInfo;
+			        var cuid = '192.168.1.2';
+			        var url2 = 'http://tsn.baidu.com/text2audio?tex='+tex+'&lan=zh&cuid='+cuid+'&ctp=1&tok='+access_token;
+			        res.send(url2);
+				});
 		    }
 		});
 	}
